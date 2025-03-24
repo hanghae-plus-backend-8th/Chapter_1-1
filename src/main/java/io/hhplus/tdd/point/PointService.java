fc98eaf83dec5c172e29c1740d48aa153cb563b0
박성수 <akgkfk3@naver.com>
2025-03-24 23:22:17 +0900

    feat: 포인트 조회 기능 추가

---
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -0,0 +1,20 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PointService {
+
+    private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
+
+    public UserPoint getPoint(long userId) {
+        return userPointTable.selectById(userId);
+    }
+}
