e98809c3a352fcab4ee0279520aa40fe8b495365
박성수 <akgkfk3@naver.com>
2025-03-26 02:15:38 +0900

    feat: 포인트 충전/이용 내역 조회 기능 추가

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,9 +1,12 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -11,8 +14,13 @@ import org.springframework.stereotype.Service;
 public class PointService {
 
     private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
 
     public UserPoint getPoint(long userId) {
         return userPointTable.selectById(userId);
+    }
+
+    public List<PointHistory> getPointHistories(long userId) {
+        return pointHistoryTable.selectAllByUserId(userId);
     }
 }
