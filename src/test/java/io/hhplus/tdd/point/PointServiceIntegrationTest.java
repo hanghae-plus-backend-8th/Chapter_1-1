790ea884ee433af8213a3f2500f7511efa85705f
박성수 <akgkfk3@naver.com>
2025-03-26 22:28:51 +0900

    test: PointService 동시성 - 통합 테스트 추가

---
+++ src/test/java/io/hhplus/tdd/point/PointServiceIntegrationTest.java
@@ -0,0 +1,67 @@
+package io.hhplus.tdd.point;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.concurrent.CountDownLatch;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class PointServiceIntegrationTest {
+
+    @Autowired
+    private PointService pointService;
+
+    @DisplayName("같은 유저가 동시에 여러 번 포인트를 사용할 수 없다.")
+    @Test
+    void deductPointsConcurrently() throws InterruptedException {
+        // given
+        long userId = 1L;
+        long amount = 50_000L;
+        int repeatCount = 20;
+        CountDownLatch countDownLatch = new CountDownLatch(repeatCount);
+
+        // when
+        pointService.chargePoint(userId, amount * repeatCount);
+
+        for (int i = 0; i < repeatCount; i++) {
+            new Thread(() -> {
+                pointService.usePoint(userId, amount);
+                countDownLatch.countDown();
+            }).start();
+        }
+        countDownLatch.await();
+        UserPoint userPoint = pointService.getPoint(userId);
+
+        // then
+        assertThat(userPoint.point()).isZero();
+    }
+
+    @DisplayName("같은 유저가 동시에 여러 번 포인트를 충전할 수 없다.")
+    @Test
+    void chargePointsConcurrently() throws InterruptedException {
+        // given
+        long userId = 1L;
+        long amount = 50_000L;
+        int repeatCount = 20;
+        CountDownLatch countDownLatch = new CountDownLatch(repeatCount);
+
+        // when
+        for (int i = 0; i < repeatCount; i++) {
+            new Thread(() -> {
+                pointService.chargePoint(userId, amount);
+                countDownLatch.countDown();
+            }).start();
+        }
+        countDownLatch.await();
+        UserPoint userPoint = pointService.getPoint(userId);
+
+        // then
+        assertThat(userPoint.point()).isEqualTo(amount * repeatCount);
+    }
+}
